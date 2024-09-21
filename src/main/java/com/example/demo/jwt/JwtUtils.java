1171a9dce688ba2393eac8e447ffc29da41bb376
magnata19 <dehpacifico@gmail.com>
2024-09-21 16:49:40 -0300

    creating a new methods that will return keys, subject and validate the token

--- src/main/java/com/example/demo/jwt/JwtUtils.java
+++ src/main/java/com/example/demo/jwt/JwtUtils.java
@@ -1,5 +1,9 @@
 package com.example.demo.jwt;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 
@@ -13,10 +17,10 @@ import java.util.Date;
 @Slf4j
 public class JwtUtils {
 
-    public static final String JWT_BEARER = "Bearer "; // Pré-fixo do token
-    public static final String JWT_AUTHORIZATION = "Authorization"; //valor do header
-    public static final String SECRET_KEY = "a38ea1685bcaa9b816bf942227a71bec"; // chave secreta
-    public static final long EXPIRE_DAYS = 0; //variaveis long para definir tempo de expiração do token
+    public static final String JWT_BEARER = "Bearer "; // token's prefix
+    public static final String JWT_AUTHORIZATION = "Authorization"; //header's value
+    public static final String SECRET_KEY = "a38ea1685bcaa9b816bf942227a71bec"; // secrect key
+    public static final long EXPIRE_DAYS = 0; //variables to define the token's expiration.
     public static final long EXPIRE_HOURS = 0;
     public static final long EXPIRE_MINUTES = 2;
 
@@ -30,8 +34,64 @@ public class JwtUtils {
         return Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
     }
 
+    /*
+    * This method is encharged to create the expiration of the token.
+    * */
     private static Date toExpireDays(Date start) {
-        LocalDateTime localDateTime = start.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+        LocalDateTime dateTime = start.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();
+        LocalDateTime end = dateTime.plusDays(EXPIRE_DAYS).plusHours(EXPIRE_HOURS).plusMinutes(EXPIRE_MINUTES);
+        return Date.from(end.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+    public static JwtToken generateToken(String username, String role) {
+        Date issuedAt = new Date();// date of token's creation
+        Date limit = toExpireDays(issuedAt);
+
+        String token = Jwts.builder()
+                .setHeaderParam("typ", "JWT")
+                .setSubject(username)
+                .setIssuedAt(issuedAt)
+                .setExpiration(limit)
+                .signWith(generateKey(), SignatureAlgorithm.HS256)
+                .claim("role", role)
+                .compact();
+
+        return new JwtToken(token);
+    }
+
+    private static Claims getClaimsFromToken(String token) {
+        try {
+            return Jwts.parser()
+                    .setSigningKey(generateKey())
+                    .build()
+                    .parseClaimsJws(refactorToken(token)).getBody();
+        } catch (JwtException ex) {
+            log.error(String.format("Token invalid: %s", ex.getMessage()));
+        }
         return null;
+    }
+
+    public static String getUsernameFromToken (String token) {
+        return getClaimsFromToken(token).getSubject();
+    }
+
+    public static boolean isTokenValid (String token) {
+        try {
+            Jwts.parser()
+                    .setSigningKey(generateKey())
+                    .build()
+                    .parseClaimsJws(refactorToken(token));
+            return true;
+        } catch (JwtException ex) {
+            log.error(String.format("Token is invalid: %s", ex.getMessage()));
+        }
+        return false;
+    }
+
+    private static String refactorToken(String token) {
+        if(token.contains(JWT_BEARER)) {
+            return token.substring(JWT_BEARER.length());
+        }
+        return token;
     }
 }
